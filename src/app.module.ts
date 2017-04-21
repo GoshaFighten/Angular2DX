57652127e116422826918981eaed2a1db22f89ec
NikitaKunashov <nikita.kunashov@users.noreply.github.com>
2017-04-21 19:46:33 +0300

    simplifing the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./components/app/app.component";
 import { HomeComponent } from "./components/home/home.component";
-import { CounterComponent } from "./components/counter/counter.component";
-import { SettingsComponent } from "./components/settings/settings.component";
 import { DevExtremeModule } from "devextreme-angular";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppTranslateModule } from "./app-translate.module";
 
 @NgModule({
     imports: [
-        AppTranslateModule,
-        AppRoutingModule,
         BrowserModule,
         DevExtremeModule
     ],
     declarations: [
-        AppComponent,
-        HomeComponent,
-        CounterComponent,
-        SettingsComponent
+        HomeComponent
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [HomeComponent]
 })
 export class AppModule {
 }
